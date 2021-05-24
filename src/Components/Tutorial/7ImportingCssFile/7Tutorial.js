a5da9104b22ca2588bea230ee8bf7bd76b94e80f
durgeshlinearloop <durgesh@linearloop.io>
2021-05-24 23:00:55 +0530

    Importing Css File in React

--- src/Components/Tutorial/7ImportingCssFile/7Tutorial.js
+++ src/Components/Tutorial/7ImportingCssFile/7Tutorial.js
@@ -1,16 +1,13 @@
-//--------------------------------- CSS Styling & Importing CSS Files in React JS
+//  CSS Styling & Importing CSS Files in React JS
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css"; // This way we import Css file in our index.js
 
 // To Import Css file we have to write import then 'relative path of the css file'
-// In place of class attribute we have to write className 
-// Becuase class is reserve keyword.
+// In place of class attribute we have to write className, because class is reserve keyword.
+// To import Google Fonts we have to import the link in the index.html and use that as we use  in css non change.
 
-// To import Google Fonts we have to import the link in the index.html
-// and use that as usally we don in css non change.
-
-let name = "Durgesh";
+let name = "Denver";
 let link1 = "https://source.unsplash.com/user/erondu/300x300";
 let link2 = "https://source.unsplash.com/collection/190727/300x300";
 let link3 = "https://source.unsplash.com/WLUHO9A_xik/300x300";

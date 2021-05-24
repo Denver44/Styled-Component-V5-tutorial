96e856542f80df82cc699293996eafa518eef2df
durgeshlinearloop <durgesh@linearloop.io>
2021-05-24 23:02:52 +0530

    Internal Css in React

--- src/Components/Tutorial/8InternalCSS/8Tutorial.js
+++ src/Components/Tutorial/8InternalCSS/8Tutorial.js
@@ -1,21 +1,18 @@
-//--------------- CSS Styling Internal and Inline Css.
+//  CSS Styling Internal and Inline Css.
 import React from "react";
 import ReactDom from "react-dom";
 
 // So this way we have to write Internal css in react js.
-//  everything must in from of object in key and vlaue pair
-//  the value must be in 'close like this'
+// Everything must in from of object in key and value pair
+// The value must be in 'close like this'
 // The key must be in camelCase.
-
 // NO kebab-case that was earlier in css Only camelCase.
-// In place of semi-colon use comma to sperate element.
+// In place of semi-colon use comma to separate element.
 
-
-let name = "Durgesh";
+let name = "Denver";
 let link1 = "https://source.unsplash.com/user/erondu/300x300";
 let link2 = "https://source.unsplash.com/collection/190727/300x300";
 let link3 = "https://source.unsplash.com/WLUHO9A_xik/300x300";
-
 
 //  Making A object we use internal Css.
 const heading = {

80443464a95607e45d726cf04d861cbc5cdceb9f
durgeshlinearloop <durgesh@linearloop.io>
2021-05-26 01:03:42 +0530

    unnecessary tutorial removed

--- src/Components/Tutorial/52UseContextHook/CompB.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import CompC from "./CompC";
-import { Fname, Lname } from "./App"; // must be in a comma as same as u export.
-
-const CompB = () => {
-  return <CompC></CompC>;
-};
-
-export default CompB;

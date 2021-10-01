d907dd551ff116450b7af17fb3356ce7abed544a
Denver <70090559+Denver44@users.noreply.github.com>
2021-10-01 06:53:47 +0530

    UseEffect and Tutorial branch created

--- src/Components/Tutorial/1ImportFile/1OldWay.js
+++
@@ -1,3 +0,0 @@
-// OLD Way to import File
-var React = require("react");
-var ReactDOM = require("react-dom");
--- src/Components/Tutorial/1ImportFile/2ModernWay.js
+++
@@ -1,8 +0,0 @@
-// As babel is inbuilt so now we can use new import keyword feature in place of require to import file.
-// New way to import files.
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-// ReactDOM.render('What to show', 'Where to show', 'Callback Function');
-ReactDOM.render(<h1> Hello World!! </h1>, document.getElementById("root"));
